0b8f4a34084f29e68169c4966ccf7a5ac85ac3f8
Amadeus <amadeusarkhammorningstar@gmail.com>
2020-09-07 16:14:05 +0600

    this is my first commit

---
+++ JsBasics/js/script.js
@@ -0,0 +1,20 @@
+'using strict';
+// const object = {
+//     obj1 : 10,
+//     obj2 : 20
+// };
+// object.obj1 = object.obj1 + object.obj2;
+// console.log(object); 
+
+
+// var name = 'Amadeus';
+
+// {
+//     let anotherNumber = 100;
+// }
+//  const answerObject = {
+//      name : prompt("name of sword?"),
+//      type : prompt("Type of sword?"),
+//      damage : prompt("Damage?")
+//  };
+ 
---
+++ JsBasics/js/sept5th.js
@@ -0,0 +1,4 @@
+'using strict';
+//interpolation ES6
+const category = 'games';
+console.log(`https://playstationstore.com/${category}/5`);
